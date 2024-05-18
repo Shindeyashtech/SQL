d8bdffb32d4d72994fe8613052ca48b6473d458c
Yash shinde <yashshinde.tech@gmail.com>
2024-05-18 20:45:15 +0530

    Day 1

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+ create database yyash
+use yyash 
+create table studentProfile2(stud_id int primary key,
+firstname varchar(20),lastname varchar(20),DOB date  ) 
+
+
+insert into studentProfile1 values (009,'yashggg','shinde','2008-10-11')
+select *from studentProfile1
+
+ drop table studentProfile
+ drop table 
