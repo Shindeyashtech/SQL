bc1bf062c831972fa519f14a5e1b375a7fee765c
Yash shinde <yashshinde.tech@gmail.com>
2024-05-24 23:50:27 +0530

    day4

---
+++ SQLQuery4.sql
@@ -0,0 +1,29 @@
+create database dayfour;
+use dayfour;
+CREATE TABLE employee (
+    employee_id INT  PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    position VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+INSERT INTO employee (employee_id, first_name, last_name, position, salary) VALUES
+(19, 'John', 'Do', 'Manager', 70000.00),
+(18, 'Emily', 'Davis', 'Designer', 75000.00),
+(17, 'Emily', 'Dav', 'Designer', 75000.00),
+(16, 'John', 'Doe', 'Manager', 70000.00),
+(1, 'John', 'Doe', 'Manager', 70000.00),
+(2, 'Jane', 'Smith', 'Developer', 60000.00),
+(3, 'Michael', 'Brown', 'Developer', 60000.00),
+(4, 'Emily', 'Davis', 'Designer', 55000.00),
+(5, 'Daniel', 'Miller', 'Manager', 70000.00),
+(6, 'Sophia', 'Wilson', 'Intern', 30000.00),
+(7, 'James', 'Moore', 'Developer', 62000.00),
+(8, 'Olivia', 'Taylor', 'Designer', 58000.00),
+(9, 'William', 'Anderson', 'Developer', 60000.00),
+(10, 'Ava', 'Thomas', 'Intern', 30000.00),
+(11, 'Liam', 'Jackson', 'Developer', 62000.00),
+(12, 'Isabella', 'White', 'HR', 50000.00),
+(13, 'Ethan', 'Harris', 'HR', 51000.00),
+(14, 'Mia', 'Martin', 'Designer', 57000.00),
+(15, 'Mason', 'Thompson', 'Developer', 60000.00);
