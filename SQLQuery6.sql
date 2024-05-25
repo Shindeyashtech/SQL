a9e63686cb3524c5c441a461e2f04c20b2261aae
Yash shinde <yashshinde.tech@gmail.com>
2024-05-25 23:42:51 +0530

    full join

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -74,3 +74,16 @@ orders.orderdetails
 from orders 
 right JOIN  customer 
 on orders.c_id = customer.c_id;
+
+
+
+select orders.order_id,
+customer.c_name,
+customer.c_id,
+orders.orderdetails 
+from orders 
+full JOIN  customer 
+on orders.c_id = customer.c_id;
+--its show only connected data
+
+
