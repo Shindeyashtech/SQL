8d5df07c1d3e9135ce46496f76e8bc53f33f97ad
Yash shinde <yashshinde.tech@gmail.com>
2024-05-27 09:27:57 +0530

    day 6

---
+++ SQLQuery7.sql
@@ -0,0 +1,16 @@
+create database daysix;
+use daysix;
+
+create table Schooldetails (name varchar(20),RollNo int primary key,address varchar(20),contact int)
+insert into Schooldetails values
+ ('raj',20,'Nanded',738740948),
+('mayur',11,'nashik',123456789),
+ ('aisha', 12, 'mumbai', 987654321),
+('rahul', 13, 'pune', 234567890),
+('simran', 14, 'delhi', 345678901),
+('vivek', 15, 'bangalore', 456789012),
+('anjali', 16, 'hyderabad', 567890123),
+('sahil', 17, 'kolkata', 678901234),
+('priya', 18, 'chennai', 789012345)
+  
+
