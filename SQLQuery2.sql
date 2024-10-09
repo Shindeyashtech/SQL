48d37314a25f7f552a3b26794958fd0b80e42db3
Yash shinde <yashshinde.tech@gmail.com>
2024-10-10 00:00:45 +0530

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,7 +1,7 @@
    create database yyash
 use yyash 
 create table studentProfile2(stud_id int primary key,
-firstname varchar(20),lastname varchar(20),DOB date  ) 
+   firstname varchar(20),lastname varchar(20),DOB date  ) 
 insert into studentProfile1 values (009,'yashggg','shinde','2008-10-11')
 insert into studentProfile1 values (004,'yaggg',null,'2008-11-11')
 insert into studentProfile1 values(006,'yaggg',null,'2008-11-11'),
